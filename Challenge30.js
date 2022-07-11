f67fc7f0b9ef2b9008ecfe4ca827588320a564f3
Lucimara Souza <lucimarasouzah@gmail.com>
2022-07-11 17:23:05 -0300

    Closure, style no js, classList

---
+++ Challenge30.js
@@ -0,0 +1,10 @@
+// Closure
+// Cada função tem um escopo fechado
+
+// Style
+// style.width = '100px'
+// style.backgroundColor = 'red'
+
+// element.classList.add()
+// element.classList.remove()
+// element.classList.toggle()
